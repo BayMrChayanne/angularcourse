196403c823e36753ded92eb66767304a7fc6df0e
BayMrChayanne <jaasielg05@gmail.com>
2025-04-23 23:59:28 -0600

    leccion 31 completada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,66 +1,74 @@
-import { CurrencyPipe, DatePipe, NgFor, UpperCasePipe, NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { CurrencyPipe, DatePipe, NgFor, UpperCasePipe, NgOptimizedImage, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListadoPeliculasComponent } from "./peliculas/listado-peliculas/listado-peliculas.component";
 
 @Component({
   selector: 'app-root',
   standalone: true, //Hay que poner esta linea de codigo para que tome los pipes como stand alone
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgFor, NgOptimizedImage],
+  imports: [ListadoPeliculasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-peliculas';
-  nombre = 'Sofia';
-  edad = 29;
-  peliculas = [
-    {
-      titulo : 'spiderman',
-      fecha_lanzamiento : new Date(),
-      precio : 1499.99,
-      poster : 'https://www.sonypictures.com.mx/sites/mexico/files/2022-01/SNH_LAS_OnLine_1400x2075.jpg'
-    },
-    {
-      titulo : 'Moana',
-      fecha_lanzamiento : new Date("2016-05-06"),
-      precio : 300.99,
-      poster : 'https://static.wikia.nocookie.net/disney/images/7/76/Moana_official_poster.jpg'
-    },
-    {
-      titulo : 'Interestellar',
-      fecha_lanzamiento : new Date("2012-09-12"),
-      precio : 100.99,
-      poster : 'https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg'
-    },
-    {
-      titulo: 'Bad Boys: Ride or Die',
-      fechaLanzamiento: new Date('2016-05-03'),
-      precio: 300.99,
-      poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg'
-    },
-    {
-      titulo: 'Deadpool & Wolverine',
-      fechaLanzamiento: new Date('2016-05-03'),
-      precio: 300.99,
-      poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/220px-Deadpool_%26_Wolverine_poster.jpg'
-    },
-    {
-      titulo: 'Oppenheimer',
-      fechaLanzamiento: new Date('2016-05-03'),
-      precio: 300.99,
-      poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/220px-Oppenheimer_%28film%29.jpg'
-    },
-    {
-      titulo: 'The Flash',
-      fechaLanzamiento: new Date('2016-05-03'),
-      precio: 300.99,
-      poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/220px-The_Flash_%28film%29_poster.jpg'
-    }
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    setTimeout(()=>{
+      this.peliculasEnCines = [
+        {
+          titulo : 'spiderman',
+          fecha_lanzamiento : new Date(),
+          precio : 1499.99,
+          poster : 'https://www.sonypictures.com.mx/sites/mexico/files/2022-01/SNH_LAS_OnLine_1400x2075.jpg'
+        },
+        {
+          titulo : 'Moana',
+          fecha_lanzamiento : new Date("2016-05-06"),
+          precio : 300.99,
+          poster : 'https://static.wikia.nocookie.net/disney/images/7/76/Moana_official_poster.jpg'
+        },
+        {
+          titulo : 'Interestellar',
+          fecha_lanzamiento : new Date("2012-09-12"),
+          precio : 100.99,
+          poster : 'https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg'
+        }
+      ];
 
-  ];
+      this.peliculasProximosEstrenos = [
+        {
+          titulo: 'Bad Boys: Ride or Die',
+          fechaLanzamiento: new Date('2016-05-03'),
+          precio: 300.99,
+          poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg'
+        },
+        {
+          titulo: 'Deadpool & Wolverine',
+          fechaLanzamiento: new Date('2016-05-03'),
+          precio: 300.99,
+          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/220px-Deadpool_%26_Wolverine_poster.jpg'
+        },
+        {
+          titulo: 'Oppenheimer',
+          fechaLanzamiento: new Date('2016-05-03'),
+          precio: 300.99,
+          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/220px-Oppenheimer_%28film%29.jpg'
+        },
+        {
+          titulo: 'The Flash',
+          fechaLanzamiento: new Date('2016-05-03'),
+          precio: 300.99,
+          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/220px-The_Flash_%28film%29_poster.jpg'
+        }
+      ];
+    }, 2000);
 
-  duplicarNumero(valor:number) : number{
-    return valor*2;
   }
+  peliculasEnCines !: any[];
+  peliculasProximosEstrenos !: any[];
+
+  clickBoton(){
+    alert("me clickearon")
+  }
+
 }
-// Mover la carpeta del curso fuera de OndeDrive
+
---
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -0,0 +1,29 @@
+import { CurrencyPipe, DatePipe, NgFor, NgIf } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-peliculas',
+  standalone : true,
+  imports: [DatePipe, CurrencyPipe, NgFor, NgIf],
+  templateUrl: './listado-peliculas.component.html',
+  styleUrl: './listado-peliculas.component.css'
+})
+export class ListadoPeliculasComponent {
+
+
+  @Input({required: true})
+  peliculas !: any[];
+
+  agregarPelicula(){
+    this.peliculas.push({
+      titulo : 'Inception',
+      fecha_lanzamiento : new Date('2012-07-03'),
+      precio : 500
+    })
+  }
+
+  remover(pelicula: any){
+    const indice = this.peliculas.findIndex((peliculaActual:any) => peliculaActual.titulo === pelicula.titulo);
+    this.peliculas.splice(indice, 1);
+  }
+}
